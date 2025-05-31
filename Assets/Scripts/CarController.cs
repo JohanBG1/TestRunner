73a96866a1c8b80124ee87d2e245f89620d68972
johan berg goettsche <johan.berg.goettsche@gmail.com>
2025-05-31 12:10:08 +0200

    Created UI And Improved CarController with rotation

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,20 +1,33 @@
 using UnityEngine;
+using TMPro;
 
 public class CarController : MonoBehaviour
 {
-    [SerializeField] float moveSpeed;
+    [SerializeField] float RotateSpeed;
+    [SerializeField] float MoveSpeed;
+    [SerializeField] TMP_Text winsText;
     private int wins = 0;
 
     private void Update()
     {
+        winsText.text = "Wins: " + wins.ToString();
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
         {
-            transform.Translate(0, 1 * moveSpeed * Time.deltaTime, 0);
+            transform.Translate(0, 1 * MoveSpeed * Time.deltaTime, 0);
         }
 
         else if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
         {
-            transform.Translate(0, 1 * moveSpeed * Time.deltaTime, 0);
+            transform.Translate(0, 1 * MoveSpeed * Time.deltaTime, 0);
+        }
+
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(0, 0, 1 * RotateSpeed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(0, 0, -1 * RotateSpeed * Time.deltaTime);
         }
     }
 
